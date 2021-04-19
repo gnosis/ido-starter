e8cb522022d49cf7e020ebcaa61648775c84dcc2
Maximiliano Molina <hpmaxi@gmail.com>
2021-04-19 12:13:25 -0300

    XDAI support using JSON-RPC provider (#38)
    * XDAI support using JSON-RPC provider
    
    * XDAI RPC env variable

--- src/networkConfig.ts
+++ src/networkConfig.ts
@@ -1,3 +1,5 @@
+import { ethers } from 'ethers'
+
 import { Networks } from '@gnosis.pm/safe-apps-sdk'
 
 const EASY_AUCTION_MAINNET_ADDRESS = process.env.REACT_APP_EASY_AUCTION_MAINNET_ADDRESS || ''
@@ -17,4 +19,16 @@ export const EASY_AUCTION_ADDRESSES: EasyAuctionAdresses = {
   KOVAN: '',
   ENERGY_WEB_CHAIN: '',
   VOLTA: '',
+}
+
+const INFURA_ID = process.env.REACT_APP_INFURA_ID
+if (!INFURA_ID) {
+  throw new Error('REACT_APP_INFURA_ID not provided')
+}
+
+const XDAI_RPC = process.env.REACT_APP_XDAI_RPC || 'https://rpc.xdaichain.com'
+
+export const getProvider = (network: Networks) => {
+  const rpc = network === 'XDAI' ? XDAI_RPC : `https://${network}.infura.io/v3/${INFURA_ID}`
+  return new ethers.providers.JsonRpcProvider(rpc)
 }
